a4415ee2230da9eec9313bc7c989d467ce355f1c
kirillkolesnik123 <42231774+kirillkolesnik123@users.noreply.github.com>
2018-09-12 01:08:46 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  // your implementation URL:https://en.wikipedia.org/wiki/Trailing_zero : ащ==for every n!= n/5^i, where 5^i< n
+   let result = 0,
+    i = 1;
+    while (Math.pow(5,i) < number){
+        result += Math.floor(number/Math.pow(5,i));
+        i++;
+    }
+    return result;
 }
